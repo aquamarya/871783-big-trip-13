21289d0dc8bcb1f57408fd797cb5d092d6715a77
Maria Vlasova <vlasova_ya@mail.ru>
2020-11-21 00:48:10 +0400

    module1-task3
    создает модули

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import {createTripInfoTemplate} from "./view/trip-info";
+import {createTripCostTemplate} from "./view/trip-cost";
+import {createMenuTemplate} from "./view/menu";
+import {createFiltersTemplate} from "./view/filters";
+import {createSortingTemplate} from "./view/sorting";
+import {createTripEventListTemplate} from "./view/trip-event-list";
+import {createTripEventItemTemplate} from "./view/trip-event-item";
+import {createEditEventTemplate} from "./view/edit-event";
+import {createNewEventTemplate} from "./view/new-event";
+
+const EVENT_AMOUNT = 3;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripMain = document.querySelector(`.trip-main`);
+render(tripMain, createTripInfoTemplate(), `afterbegin`);
+
+const tripInfo = document.querySelector(`.trip-info`);
+render(tripInfo, createTripCostTemplate(), `beforeend`);
+
+const tripControls = document.querySelector(`.trip-controls`);
+render(tripControls, createMenuTemplate(), `beforeend`);
+render(tripControls, createFiltersTemplate(), `beforeend`);
+
+const pageMain = document.querySelector(`.page-main`);
+const tripEvents = pageMain.querySelector(`.trip-events`);
+render(tripEvents, createSortingTemplate(), `beforeend`);
+render(tripEvents, createEditEventTemplate(), `beforeend`);
+render(tripEvents, createTripEventListTemplate(), `beforeend`);
+
+const eventsList = document.querySelector(`.trip-events__list`);
+
+for (let i = 0; i < EVENT_AMOUNT; i++) {
+  render(eventsList, createTripEventItemTemplate(), `beforeend`);
+}
+
+render(eventsList, createEditEventTemplate(), `afterbegin`);
+render(eventsList, createNewEventTemplate(), `afterbegin`);
